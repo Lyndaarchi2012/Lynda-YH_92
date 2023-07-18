664d1e04d0fd4bc159316dee5379c980a3016a20
Lynda YH <139888872+Lyndaarchi2012@users.noreply.github.com>
2023-07-18 16:59:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var nav = document.getElementById('navbar');
+var hamburger = document.getElementById('hamburger-icon');
+
+
+function ouvrirMenu () {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+
+}; 
